e008895db6221c53bb5ec69f78ab51fe04510e32
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-11-10 15:50:31 -0600

    Added notes

--- script.js
+++ script.js
@@ -14,22 +14,23 @@ $(function () {
   // useful when saving the description in local storage?
   //
   // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes?
+  // block by comparing the id to the current hour. 
+  
   // Used day.js to get the current hour in 24-hour time
   let currentHour = dayjs().hour();
   console.log(currentHour)
 
+  // Compared the id to the current hour
   for ( let i = 0; i < currentHour.length; i++) {
     currentHour[i].removeClass("past present future");
-    
-    if ( currentDayEl > currentHour[i].data("hour")) {
-      currentHour[i].addClass("past");
+
+  // Used ID attribute to each time-block to conditionally add or remove the past, present, and future classes 
+    if ( currentDayEl > currentHour[i].data(".hour")) {
+      currentHour[i].addClass(".past");
     } else if ( currentDayEl === currentHour[i].attr("data-hour")) {
-      currentHour[i].addClass("present");
+      currentHour[i].addClass(".present");
     } else {
-      currentHour[i].addClass("future");
+      currentHour[i].addClass(".future");
     }
   };
   // TODO: Add code to get any user input that was saved in localStorage and set
