e22f620f4421b3dacfaf638202ca0e26a2d378d5
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-11-10 13:03:26 -0600

    Added README template

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let currentDayEl = document.getElementById("currentDay")
-console.log(currentDayEl)
-console.log(dayjs().format('dddd, MMMM DD'));
+
 currentDayEl.textContent = dayjs().format('dddd, MMMM DD');
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
@@ -19,6 +18,8 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+  let currentHour = dayjs().hour();
+  console.log(currentHournom)
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
