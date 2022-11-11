819185b85e4832c388beda8895f78b46a9244247
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-11-10 22:09:08 -0600

    Added attribute to div tag and for loop

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// Displays the current date in he header of the page
-let currentDayEl = document.getElementById("currentDay")
-
-currentDayEl.textContent = dayjs().format('dddd, MMMM DD');
-
 // Used day.js to get the current hour in 24-hour time
 let currentHour = dayjs().hour();
+console.log(currentHour)
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  // Displays the current date in he header of the page
+let currentDayEl = document.getElementById("currentDay")
+let timeEl = document.getElementsByClassName("time-block");
+
+currentDayEl.textContent = dayjs().format('dddd, MMMM DD');
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -19,31 +21,24 @@ $(function () {
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. 
-  let currentHour = (new Date()).getHours();
-  $('.time-block')
-    .each (function() {
-      let val = parseInt($(this).prop('id'));
-      if (val > currentHour && val < currentHour + 6) {
-        $(this).css('.past');
-        console.log()
-      }
-    })
-  
-
 
   // Compared the id to the current hour
-  // for ( let i = 0; i < currentHour.length; i++) {
-  //   currentHour[i].removeClass("past present future");
+  for ( let i = 0; i < timeEl.length; i++) {
+    timeEl[i].getAttribute("data-hour");
+    console.log(timeEl[i].getAttribute("data-hour"))
+    
 
   // Used ID attribute to each time-block to conditionally add or remove the past, present, and future classes 
-  //   if ( currentDayEl > currentHour[i].data(".hour")) {
-  //     currentHour[i].addClass(".past");
+  //   if ( currentDayEl > currentHour[i].data("hour")) {
+  //     currentHour[i].addClass("past");
   //   } else if ( currentDayEl === currentHour[i].attr("data-hour")) {
-  //     currentHour[i].addClass(".present");
+  //     currentHour[i].addClass("present");
   //   } else {
-  //     currentHour[i].addClass(".future");
-  //   }
+  //     currentHour[i].addClass("future");
+    }
   // };
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
